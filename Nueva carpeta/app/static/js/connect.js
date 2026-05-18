753fd21b788ff69bf4f99d83c34d81af7e958d8c
Fbyg <96188037+Fbyg@users.noreply.github.com>
2026-05-18 12:41:03 +0200

    Add files via upload

--- Nueva carpeta/app/static/js/connect.js
+++ Nueva carpeta/app/static/js/connect.js
@@ -1,26 +1,40 @@
 function pauseApp() {
     const overlay = document.getElementById("offlineOverlay");
-
     if (overlay) overlay.classList.remove("hidden");
-
     document.body.style.pointerEvents = "none";
 }
+
 function resumeApp() {
     const overlay = document.getElementById("offlineOverlay");
-
     if (overlay) overlay.classList.add("hidden");
-
     document.body.style.pointerEvents = "auto";
 }
+
 function showOnlinePopup() {
-
     const popup = document.getElementById("onlinePopup");
-
     if (!popup) return;
 
     popup.classList.remove("hidden");
 
     setTimeout(() => {
         popup.classList.add("hidden");
-    }, 4000);
+    }, 2000);
 }
+
+window.addEventListener("offline", () => {
+    pauseApp();
+});
+
+window.addEventListener("online", async () => {
+    // primero restauras UI SIEMPRE
+    resumeApp();
+    showOnlinePopup();
+
+    // luego validas token (opcional)
+    try {
+        await keycloak.updateToken(5);
+    } catch (e) {
+        console.warn("Token inválido tras reconexión");
+        keycloak.login();
+    }
+});
